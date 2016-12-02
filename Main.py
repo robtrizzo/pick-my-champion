1288a2a09511f0a4320857a9764410d1ef8c3193
robtrizzo <robtrizzo@gmail.com>
2016-12-01 20:56:15 -0500

    removing main.py

--- Main.py
+++
@@ -1,9 +0,0 @@
-from RiotAPI import RiotAPI
-
-def main():
-	api = RiotAPI('RGAPI-a385434d-2a02-40fe-8fdb-df67d38a506f')
-	r = api.get_summoner_by_name('SpookyDaMoose')
-	print(r)
-
-if __name__ == "__main__":
-	main()
