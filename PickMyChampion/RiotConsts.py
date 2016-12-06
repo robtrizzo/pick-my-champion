a34449132b66bbb503246708faf1b89a5f7eb229
robtrizzo <robtrizzo@gmail.com>
2016-12-05 23:02:44 -0500

    added capability to pull champion info
    allows us to pull all champion info and parse it into a 2D array of champion names and IDs for easier access

--- PickMyChampion/RiotConsts.py
+++ PickMyChampion/RiotConsts.py
@@ -1,10 +1,13 @@
 URL = {
 	'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
-	'summoner_by_name': 'v{version}/summoner/by-name/{names}'
+	'base_static': 'https://global.api.pvp.net/api/lol/static-data/{url}',
+	'summoner_by_name': 'v{version}/summoner/by-name/{names}',
+	'all_champion_info': '{region}/v{version}/champion?champData=info'
 }
 
 API_VERSIONS = {
-	'summoner': '1.4'
+	'summoner': '1.4',
+	'lol-static-data': '1.2'
 }
 
 REGIONS = {
