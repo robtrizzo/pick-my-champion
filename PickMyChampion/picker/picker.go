d3c44e4dd437f339b3bf0f1ab07d0d92c4603fa7
David Kindel <dkindel@vt.edu>
2016-12-07 21:34:11 -0500

    Dynamically add champions via the portrait dir.  Now to make them draggable...

--- PickMyChampion/picker/picker.go
+++ PickMyChampion/picker/picker.go
@@ -2,10 +2,61 @@ package picker
 
 import (
 	"net/http"
+	"path/filepath"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"os"
 )
 
 func init() {
 	http.Handle("/picker/", http.StripPrefix("/picker/", http.FileServer(http.Dir("picker/templates"))))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("picker/scripts"))))
+	http.Handle("/scripts/php", http.StripPrefix("/scripts/php", http.FileServer(http.Dir("picker/scripts/php"))))
+	http.HandleFunc("/scripts/go/", goScriptHandler)
 }
+
+func goScriptHandler(w http.ResponseWriter, r *http.Request) {
+	base := filepath.Base(r.RequestURI)
+
+	//map the base name to a function
+	//expand this list as functions are added
+	m := map[string]func(http.ResponseWriter, *http.Request){
+		"listDir": listDir,
+	}
+
+	if f, ok := m[base]; ok {
+		f(w, r)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func listDir(w http.ResponseWriter, r *http.Request) {
+	dir := r.FormValue("dir_path")
+	topLevel := strings.Split(dir, "/")[1]
+	if topLevel != "static" {
+		http.Error(w, "Content can only be listed if a child of the static directory (/static.  Don't forget the " +
+				"leading \"/\"). Passed dir: " + topLevel,
+			http.StatusNotImplemented)
+		return
+	}
+	final_dir := strings.TrimPrefix(filepath.FromSlash(dir), string(filepath.Separator))
+	abs, _ := filepath.Abs(final_dir)
+	cwd, _ := os.Getwd()
+	if !strings.HasPrefix(abs, filepath.Join(cwd, "static")) {
+		http.Error(w, "Attempted to read outside the static directory.", http.StatusInternalServerError)
+		return
+	}
+	files, err := ioutil.ReadDir(final_dir)
+	if err != nil{
+		http.Error(w, "Error reading dir " + final_dir + ": " + err.Error(), http.StatusInternalServerError)
+	}
+	for i, fn := range files {
+		if i != 0 {
+			fmt.Fprint(w, ",")
+		}
+		fmt.Fprint(w, fn.Name())
+	}
+}
+
