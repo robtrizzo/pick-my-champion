df5d4c085af716e9b3feeb050fdc4ad3433c8012
David Kindel <dkindel@vt.edu>
2016-12-08 21:49:25 -0500

    Reorganize again for the sake of go getting new code

--- PickMyChampion/picker/router.go
+++ PickMyChampion/picker/router.go
@@ -2,16 +2,16 @@ package picker
 
 import (
 	"net/http"
-	"picker/scripts/golang"
+	"goscripts"
 )
 
 func init() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("templates"))))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.Handle("/scripts/js/", http.StripPrefix("/scripts/js/", http.FileServer(http.Dir("picker/scripts/js"))))
+	http.Handle("/scripts/js/", http.StripPrefix("/scripts/js/", http.FileServer(http.Dir("scripts/js"))))
 	http.HandleFunc("/scripts/go/", goScriptHandler)
 }
 
 func goScriptHandler(w http.ResponseWriter, r *http.Request) {
-	golang.ScriptHandler(w,r)
+	goscripts.ScriptHandler(w,r)
 }
